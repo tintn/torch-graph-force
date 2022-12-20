f9b1d8d3cd6daf64c18f03856e5e9d41fdbe9be0
tin <trung.tin.nguyen0309@gmail.com>
2022-12-20 18:27:54 +0700

    Fix imports

--- torch_graph_force/__init__.py
+++ torch_graph_force/__init__.py
@@ -1,3 +1,3 @@
-from torch_force_graph.data import from_networkx, from_edgelist, from_pandas_dataframe
+from torch_graph_force.data import from_networkx, from_edgelist, from_pandas_dataframe
 
-from torch_force_graph.layout import spring_layout
+from torch_graph_force.layout import spring_layout
